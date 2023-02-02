6551f1a4a89e7e286f0b688b16acbdbd21ebfec2
Sri Vishnu <54411695+devish99@users.noreply.github.com>
2023-02-02 08:37:58 +0530

    Changed function name

--- Calculator/MainActivity.kt
+++ Calculator/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     var num1=0
     var num2=0
-    var cal = calc(num1,num2)
+    var cal = calculate(num1,num2)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
